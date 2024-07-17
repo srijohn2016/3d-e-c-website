4a06fc62a95526eb4db97544ef001b6b7233a9d0
SRISUDHARSAN R <srisudharsan1620@gmail.com>
2024-07-17 17:09:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// const navLinkEls=document.querySelectorAll('nav-link');
+
+// navLinkEls.forEach(navlinkEl=>{
+//     navlinkEl.addEventListener('click',function(){
+//         document.querySelector('active')?.classList.remove('active');
+//         this.classList.add('active');
+//     });
+
+// });
+
+// const li=document.querySelectorAll(".nav-item");
+// const sec=document.querySelectorAll("section");
+
+// function activeMenu(){
+//     let len=sec.length;
+//     while(--length && window.scrollY +97 < sec[len].offsetTop){}
+//     li.forEach(ltx=> ltx.classList.remove("active"));
+//     li[len].classList.add("active");
+// }
+// activeMenu();
+// window.addEventListener("scroll",activeMenu);
